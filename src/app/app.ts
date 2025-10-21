a4822e651020561ebf69c88d2253c1d80756fb4a
Devi Manikanta <manikantapalaparthi@gmail.com>
2025-10-21 18:14:33 +0530

    fix cors issue

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import expenseRoutes from '../router/expenseRoutes';
 import { errorHandler } from '../middleware/errorHandler';
-
+import cors from 'cors'
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/expenses', expenseRoutes);
 app.use(errorHandler);
